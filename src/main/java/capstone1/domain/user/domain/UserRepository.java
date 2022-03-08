67cae43c94da7e9cdf0cc95855eafff3fa5dd1b9
Kwon <pawoo0211@naver.com>
2022-03-08 13:23:40 +0900

    add domain user test git push alarm in slack

---
+++ src/main/java/capstone1/domain/user/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package capstone1.domain.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
--- src/main/java/capstone1/domain/user/domain/UserRespository.java
+++
@@ -1,4 +0,0 @@
-package capstone1.domain.user.domain;
-
-public interface UserRespository {
-}
